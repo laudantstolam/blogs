12a9d1599f000a96d4af0845bc40cbabc9231ea9
laudantstolam <rachelle0607@gmail.com>
2025-07-03 12:59:43 +0800

    [feat] adding Latex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import expressiveCode from 'astro-expressive-code';
-import remarkMermaid from 'remark-mermaid';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
+import remarkMermaid from "remark-mermaid";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://laudantstolam.github.io',
-  base: '/blogs/',
+  site: "https://laudantstolam.github.io",
+  base: "/blogs/",
   markdown: {
-    remarkPlugins: [[remarkMermaid, {
-      simple: true,           // Use simpler renderer
-      wrap: null,            // Don't wrap the output
-      mermaidConfig: {
-        theme: 'default',
-        securityLevel: 'mediums',
-        startOnLoad: true
-      }
-    }]]
+    remarkPlugins: [
+      [
+        remarkMermaid,
+        {
+          simple: true, // Use simpler renderer
+          wrap: null, // Don't wrap the output
+          mermaidConfig: {
+            theme: "default",
+            securityLevel: "mediums",
+            startOnLoad: true,
+          },
+        },
+      ],
+      remarkMath,
+    ],
+    rehypePlugins: [rehypeKatex] 
   },
-  integrations: [
-    expressiveCode(),
-    mdx(),
-    sitemap(),
-    tailwind()
-  ],
-  output: 'static',
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind()],
+  output: "static",
 });
