3b40177e8349bb8cf7199e943899c8160ea27e57
laudantstolam <rachelle0607@gmail.com>
2026-07-25 00:39:56 +0800

    [feat] adding RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { getUrl } from '../utils/url';
 
@@ -8,18 +9,24 @@ export async function GET(context) {
 		return data.publish !== false;
 	});
 
+	posts.sort((a, b) => (b.data.created_date?.valueOf() ?? 0) - (a.data.created_date?.valueOf() ?? 0));
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description || '',
-			pubDate: post.data.created_date || new Date(),
-			link: getUrl(`/${post.id}/`),
-			// Optional fields
-			author: post.data.author,
-			categories: post.data.tags || [],
-		})),
+		items: posts.map((post) => {
+			const slug = post.data.slug || post.id.replace(/\.[^/.]+$/, '');
+			return {
+				title: post.data.title,
+				description: post.data.description || '',
+				pubDate: post.data.created_date || new Date(),
+				link: getUrl(`/${slug}/`),
+				content: sanitizeHtml(post.rendered?.html || ''),
+				// Optional fields
+				author: post.data.author,
+				categories: post.data.tags || [],
+			};
+		}),
 	});
 }
